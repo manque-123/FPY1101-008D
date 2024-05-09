e00a2536956f8e32c50f961620eef39c50b70f94
manque-123 <168593281+manque-123@users.noreply.github.com>
2024-05-09 17:41:23 -0400

    Add files via upload

---
+++ Pruebasushi.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+contador=1
+
+os.system("cls")
+
+
+while contador<=4:
+    print('''  
+                1) Pikachu Roll $4500
+                2) Otaku Roll $5000
+                3) Pulpo Venenoso Roll $5200
+                4) Anguila Electrica Roll $4800
+                5) salir   ''') 
+    contador=int(input("ingresa la opcion   "))
+
+if opcion_1:
+    opcion=input 
+
+    perdon profe :( estudie toda la mañana :"(
